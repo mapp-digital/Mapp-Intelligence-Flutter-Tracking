d728ad588f861a8abbef6462841f3877111cdd65
Stefan <stefan.stevanovic@mapp.com>
2021-04-15 14:47:49 +0200

    remove reset and anynimous tracking features and add one example for Android webview

---
+++ example/lib/WebviewForAndroid.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebviewForAndroid extends StatelessWidget {
+  void initState() {
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    //PluginMappintelligence.trackWebview();TODO try to mapp it here
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    initState();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WebviewForAndroid'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: WebView(
+        initialUrl: 'http://demoshop.webtrekk.com/web2app/index.html',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
