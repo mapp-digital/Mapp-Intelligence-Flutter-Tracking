d1ac532224d1da191a89e730a8329ad938f62e6d
semsudin.tafilovic <sekitafilovic@gmail.com>
2023-05-15 15:51:31 +0200

    Changed MainActivity to extend from FlutterFragmentActivity insted of FlutterActivity

--- example/android/app/src/main/kotlin/com/example/plugin_mappintelligence_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/plugin_mappintelligence_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.plugin_mappintelligence_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
 }
