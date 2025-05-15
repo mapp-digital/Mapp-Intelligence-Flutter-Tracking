6099ded347c029831cabd09f96ffe31e53361467
Stefan Stevanovic <stefan.stevanovic@mapp.com>
2025-05-15 14:35:32 +0200

    update version for native iOS library to 5.0.15

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/semsudin.tafilovic/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/semsudin.tafilovic/VisualCodeProjects/intelligence-flutter-plugin/example"
+export "FLUTTER_ROOT=/Users/stefan.stevanovic/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/stefan.stevanovic/Documents/Mapp-Intelligence-Flutter-Tracking/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
