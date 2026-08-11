e298c06b8324a2c0e641bdeb8c548f9372c0ecf3
tafilovic <sekitafilovic@gmail.com>
2026-08-11 14:04:11 +0200

    Update example widget test to verify the current home screen UI instead of the removed platform version text.

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,23 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:plugin_mappintelligence_example/main.dart';
 
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('renders the example home screen', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
-    // Verify that platform version is retrieved.
-    expect(
-      find.byWidgetPredicate(
-        (Widget widget) =>
-            widget is Text && widget.data!.startsWith('Running on:'),
-      ),
-      findsOneWidget,
-    );
+    expect(find.text('Mapp Intelligence Demo'), findsOneWidget);
+    expect(find.text('Configuration'), findsOneWidget);
   });
 }
